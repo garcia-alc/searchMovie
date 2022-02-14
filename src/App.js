2e274b6d0c0860a829d6b8e2e1ed9f448a7ea2f2
EnigmaOsiris <enigma.osiris@gmail.com>
2022-02-14 16:36:21 -0600

    {}

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
+import 'bulma/css/bulma.css'
+import {Title} from './components/Title'
+import {SearchFrom} from './components/SearchForm'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Title>search movies</Title>
+        <div className = 'SearchFrom-wrapper'>
+            
+        </div>
     </div>
   );
 }
